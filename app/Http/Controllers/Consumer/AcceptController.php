495e3b034f60bc818bb949a2045e78a56e9743f7
THARUN KUMAR KARANAM <17001362@MEGHA.LOCAL>
2026-09-16 17:55:51 +0530

    error rectified

--- app/Http/Controllers/Consumer/AcceptController.php
+++ app/Http/Controllers/Consumer/AcceptController.php
@@ -59,7 +59,7 @@ class AcceptController extends Controller
             $status_val = "rejected";
         }
         // Consumer Update
-        $consumer = Consumer::find($id)
+        $consumer = Consumer::find($id);
         $consumer->update([
             'status_id' => $con_status,
             'updated_by' => Auth::id(),
